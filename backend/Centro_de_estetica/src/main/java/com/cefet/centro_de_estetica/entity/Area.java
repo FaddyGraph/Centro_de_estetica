3eae28f5aaff0df79bf1c0985386c2c2fcbfaf9a
vitorolivecardk <vitorolivecard@gmail.com>
2025-12-01 23:15:32 -0300

    Add Area entity with JPA annotations
    Precisa da classe Serviço

---
+++ backend/Centro_de_estetica/src/main/java/com/cefet/centro_de_estetica/entity/Area.java
@@ -0,0 +1,46 @@
+package com.centroestetica.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore; 
+
+@Entity
+public class Area {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idArea;
+
+    private String nome; 
+
+    @OneToMany(mappedBy = "area") 
+    @JsonIgnore 
+    private List<Servico> servicos;
+
+    public Area() {
+    }
+
+    public Long getIdArea() {
+        return idArea;
+    }
+
+    public void setIdArea(Long idArea) {
+        this.idArea = idArea;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Servico> getServicos() {
+        return servicos;
+    }
+
+    public void setServicos(List<Servico> servicos) {
+        this.servicos = servicos;
+    }
+}
