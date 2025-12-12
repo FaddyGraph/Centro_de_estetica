136665fe71c544a16395ca469f72dbcdf3bc3b92
Vitor Oliveira Cardoso <vitorolivecard@gmail.com>
2025-12-12 20:01:35 -0300

    Criando DTOs para Area, Servico e Horario

---
+++ backend/Centro_de_estetica/src/main/java/com/cefet/centro_de_estetica/dto/HorarioUsuarioDTO.java
@@ -0,0 +1,66 @@
+package com.cefet.centro_de_estetica.dto;
+
+import java.time.LocalTime;
+
+import com.cefet.centro_de_estetica.entity.HorarioUsuario;
+
+public class HorarioUsuarioDTO {
+
+    private Long id;
+    private Integer diaSemana;
+    private LocalTime horario;
+    private Long idFuncionario;
+    private String nomeFuncionario;
+
+    public HorarioUsuarioDTO() {
+    }
+
+    public HorarioUsuarioDTO(HorarioUsuario horarioUsuario) {
+        this.id = horarioUsuario.getId();
+        this.diaSemana = horarioUsuario.getDiaSemana();
+        this.horario = horarioUsuario.getHorario();
+        this.idFuncionario = horarioUsuario.getFuncionario().getId();
+        this.nomeFuncionario = horarioUsuario.getFuncionario().getNome();
+    }
+
+
+    public Long getId() { 
+        return id; 
+    }
+
+    public void setId(Long id) { 
+        this.id = id; 
+    }
+
+    public Integer getDiaSemana() { 
+        return diaSemana; 
+    }
+
+    public void setDiaSemana(Integer diaSemana) { 
+        this.diaSemana = diaSemana; 
+    }
+
+    public LocalTime getHorario() { 
+        return horario; 
+    }
+
+    public void setHorario(LocalTime horario) { 
+        this.horario = horario; 
+    }
+
+    public Long getIdFuncionario() { 
+        return idFuncionario; 
+    }
+
+    public void setIdFuncionario(Long idFuncionario) { 
+        this.idFuncionario = idFuncionario; 
+    }
+
+    public String getNomeFuncionario() { 
+        return nomeFuncionario; 
+    }
+    
+    public void setNomeFuncionario(String nomeFuncionario) {
+        this.nomeFuncionario = nomeFuncionario; 
+    }
+}
---
+++ backend/Centro_de_estetica/src/main/java/com/cefet/centro_de_estetica/dto/ServicoDTO.java
@@ -0,0 +1,85 @@
+package com.cefet.centro_de_estetica.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalTime;
+
+import com.cefet.centro_de_estetica.entity.Servico;
+
+public class ServicoDTO {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private LocalTime tempoAtendimento;
+    private BigDecimal valor;
+    private Long idArea;     
+    private String nomeArea; 
+
+    public ServicoDTO() {
+    }
+
+    public ServicoDTO(Servico servico) {
+        this.id = servico.getId();
+        this.nome = servico.getNome();
+        this.descricao = servico.getDescricao();
+        this.tempoAtendimento = servico.getTempoAtendimento();
+        this.valor = servico.getValor();
+        this.idArea = servico.getArea().getIdArea();
+        this.nomeArea = servico.getArea().getNome();
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id; 
+    }
+
+    public String getNome() { 
+        return nome; 
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome; 
+    }
+
+    public String getDescricao() { 
+        return descricao; 
+    }
+
+    public void setDescricao(String descricao) { 
+        this.descricao = descricao;
+    }
+
+    public LocalTime getTempoAtendimento() {
+        return tempoAtendimento; 
+    }
+
+    public void setTempoAtendimento(LocalTime tempoAtendimento) { 
+        this.tempoAtendimento = tempoAtendimento; 
+    }
+
+    public BigDecimal getValor() { 
+        return valor; 
+    }
+
+    public void setValor(BigDecimal valor) { 
+        this.valor = valor;
+    }
+
+    public Long getIdArea() { 
+        return idArea; 
+    }
+    public void setIdArea(Long idArea) { 
+        this.idArea = idArea; 
+    }
+
+    public String getNomeArea() { 
+        return nomeArea; 
+    }
+
+    public void setNomeArea(String nomeArea) { 
+        this.nomeArea = nomeArea; 
+    }
+}
