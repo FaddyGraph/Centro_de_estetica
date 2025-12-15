43dadb062973540a0c8c43466bdd427e81783ea1
FlavioCorcini <flaviosamora06@gmail.com>
2025-12-15 18:52:47 -0300

    Criação da entidade Agendamento

--- backend/Centro_de_estetica/src/main/java/com/cefet/centro_de_estetica/entity/Agendamento.java
+++ backend/Centro_de_estetica/src/main/java/com/cefet/centro_de_estetica/entity/Agendamento.java
@@ -1,5 +1,109 @@
 package com.cefet.centro_de_estetica.entity;
 
-public class Agendamento {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
+import com.cefet.centro_de_estetica.enums.StatusAgendamento;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_agendamento")
+public class Agendamento {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idUsuario")
+    private Long id;
+	
+	// Guarda a data E a hora juntas (ex: 2025-12-25T14:30:00)
+    @Column(nullable = false)
+    private LocalDateTime dataHora;
+    
+    @Column(nullable = false)
+    private BigDecimal valorCobrado;
+    
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusAgendamento status;
+	
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", nullable = false)
+    private Usuario cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "id_funcionario", nullable = false)
+    private Usuario funcionario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_servico", nullable = false)
+    private Servico servico;
+
+    
+    //GETs & SETs
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+
+	public BigDecimal getValorCobrado() {
+		return valorCobrado;
+	}
+
+	public void setValorCobrado(BigDecimal valorCobrado) {
+		this.valorCobrado = valorCobrado;
+	}
+
+	public StatusAgendamento getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusAgendamento status) {
+		this.status = status;
+	}
+
+	public Usuario getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Usuario cliente) {
+		this.cliente = cliente;
+	}
+
+	public Usuario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Usuario funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	public Servico getServico() {
+		return servico;
+	}
+
+	public void setServico(Servico servico) {
+		this.servico = servico;
+	}
+    
 }
